c8134570afbefae23b499db2b05e52de5c7abe54
Aslanov Rustam <aslanovrg@gmail.com>
2023-11-09 18:05:09 +0200

    add audio component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,14 @@
-import { Suspense } from "react";
+import { Suspense, useRef, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import { useData } from "../../helpers/DataContext";
 import Navbar from "../Navbar/Navbar";
 import Loader from "../Loader/Loader";
 import Modal from "../Modal/Modal";
+import sound from "../../assets/music.mp3";
+import { ReactComponent as PlayIco } from "../../assets/play.svg";
 import s from "./sidebar.module.css";
-import { Toaster } from "react-hot-toast";
 
 export default function Sidebar() {
   const {
@@ -16,9 +19,25 @@ export default function Sidebar() {
     setUpdatebleTask,
   } = useData();
 
+  const [isPlaying, setIsPlaying] = useState(false);
+
   const togleModal = () => {
     setShowmodal(!showModal);
   };
+
+  const audioRef = useRef(new Audio(sound));
+
+  const toggleAudio = () => {
+    const audio = audioRef.current;
+    audio.loop = true;
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <section className={s.section}>
       <div className={`${s.sidebar} ${hideSidebar && s.hide}`}>
@@ -28,6 +47,9 @@ export default function Sidebar() {
           onClick={() => setHideSidebar(!hideSidebar)}
         >
           {hideSidebar ? "show" : "hide"}
+        </div>
+        <div className={s.player}>
+          <PlayIco className={s.playIcon} onClick={toggleAudio} />
         </div>
       </div>
       <Suspense fallback={<Loader />}>
